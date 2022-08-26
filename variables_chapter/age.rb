0b4593b5beebd21937975fb6aa237e080d1d2486
Josh John <joshuajohn2777@gmail.com>
2022-08-25 22:33:21 -0400

    exercises from variable chapter in intro book

---
+++ variables_chapter/age.rb
@@ -0,0 +1,14 @@
+puts "How old are you?"
+age = gets.chomp
+age_in_ten = age.to_i + 10
+age_in_twenty = age.to_i + 20
+age_in_thirty = age.to_i + 30
+age_in_forty = age.to_i + 40
+puts "In 10 years you will be:
+#{age_in_ten}"
+puts "In 10 years you will be:
+#{age_in_twenty}"
+puts "In 10 years you will be:
+#{age_in_thirty}"
+puts "In 10 years you will be:
+#{age_in_forty}"
---
+++ variables_chapter/name.rb
@@ -0,0 +1,9 @@
+puts "what is your first name?"
+first_name = gets.chomp
+puts "what is your last name?"
+last_name = gets.chomp
+puts first_name + " " + last_name
+
+# puts "Hello, #{name}! How's it going?"
+
+# 10.times { puts name }
