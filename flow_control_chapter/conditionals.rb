a893bf7d9852c8f67c30bf65d31cfb80892a7723
Josh John <joshuajohn2777@gmail.com>
2022-08-27 21:27:07 -0400

    two more chapters of intro to ruby complete

---
+++ flow_control_chapter/conditionals.rb
@@ -0,0 +1,14 @@
+# conditional.rb
+
+puts "Put in a number"
+a = gets.chomp.to_i
+
+if a == 4
+  puts "a equals 3"
+elsif a == 4
+  puts "a equals 4"
+else 
+  puts "a is equal to neither 3 or 4"
+end
+
+
---
+++ methods_chapter/scream.rb
@@ -0,0 +1,7 @@
+def scream(words)
+  words += "!!!!!"
+  puts words
+end
+
+scream("Yippee")
+
